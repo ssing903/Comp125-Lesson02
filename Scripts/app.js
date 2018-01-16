79b297391239b21183c8dedecf63e396ef6bcac0
ssing903 <ssing902@my.centennialcollege.ca>
2018-01-16 11:39:42 -0500

    added event

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,9 +7,19 @@
 
     "use strict";
     // code here
+   var firstPTag = document.getElementById("firstParagraph");
 
+   // event listener for click
+   firstPTag.addEventListener("click",firstParagraphClicked);
+    
+    // event listener for hover
+    firstPTag.addEventListener("mouseover",firstParagraphHover);
+    
+    var firstParagraphIsClicked = false;
+    var firstParagraphWasHover = false;
     console.log("App started");
 
+    var myFirstParagraph = "This is my first pargraph. I am writing it for demonstartion";
     /*
     // declare named function
     function writeToFirstParagraph()  {
@@ -29,14 +39,44 @@
         // debugging only
        console.log("Executing the writeToParagraph");
        //create an reference to p element with an id of the first paragraph
-        var firstPTag = document.getElementById("firstParagraph");
-        firstPTag.textContent = "It's alive!!!";
+        
+        firstPTag.textContent = myFirstParagraph;
        // console.log(sukh);
-    }    
+      // alert("This is an alert");
+      //document.write("My document will be destroyed");
+     }    
 
    // var alias = writeToFirstParagraph;
     // execute function
     writeToFirstParagraph();
 
-    console.log(firstPTag);
+    // event handdler
+    function firstParagraphClicked()
+    {
+        if(firstParagraphIsClicked)
+        {
+            firstPTag.style.color = "#000000"; 
+            firstParagraphIsClicked = false;       
+        }
+        else
+        {
+            firstPTag.style.color = "green";
+            firstParagraphIsClicked = true;
+        }
+    }
+
+    function firstParagraphHover()
+    {
+        if(firstParagraphWasHover)
+        {
+            firstPTag.style.color = "#000000"; 
+            firstParagraphWasHover = false;       
+        }
+        else
+        {
+            firstPTag.style.color = "#FF0000";
+            firstParagraphWasHover = true;
+        }
+    }
+    //console.log(firstPTag);
 })();
