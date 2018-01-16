12e0fb6302a394cddeba5033776c7da7128e7983
ssing903 <ssing902@my.centennialcollege.ca>
2018-01-16 09:34:38 -0500

    updated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,4 +1,6 @@
 /*Script goes here */
+// global scope
+//var sukh ="Sukh"; 0 eating my zombie
 // IIFE - Immediately Invoked Function Expression
 (function(){
 
@@ -8,7 +10,33 @@
 
     console.log("App started");
 
-    //create an reference to p element with an id of the first paragraph
-    var firstParagraph = document.getElementById("firstParagraph");
-    firstParagraph.textContent = "It's alive!!!";
+    /*
+    // declare named function
+    function writeToFirstParagraph()  {
+      // debugging only
+       console.log("Executing the writeToParagraph");
+       //create an reference to p element with an id of the first paragraph
+        var firstParagraph = document.getElementById("firstParagraph");
+        firstParagraph.textContent = "It's alive!!!";
+       // console.log(sukh);
+
+    }
+    */ 
+
+    // declare a variable that point to anonomaus function
+    var writeToFirstParagraph = function()
+    {
+        // debugging only
+       console.log("Executing the writeToParagraph");
+       //create an reference to p element with an id of the first paragraph
+        var firstPTag = document.getElementById("firstParagraph");
+        firstPTag.textContent = "It's alive!!!";
+       // console.log(sukh);
+    }    
+
+   // var alias = writeToFirstParagraph;
+    // execute function
+    writeToFirstParagraph();
+
+    console.log(firstPTag);
 })();
